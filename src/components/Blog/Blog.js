9f472be007478fa0ae65c2ffe758b850b78f04e3
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-12-11 16:37:51 +0600

    blog section added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
+        //blog section
         <div>
             <h1 className='text-4xl text-center' >Coming Soon...</h1>
         </div>
