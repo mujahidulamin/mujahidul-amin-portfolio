e4054005b5d434a889b30c5791c31baaab8b6bf6
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-12-11 16:37:23 +0600

    footer-section-added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
+    //footer section
     <div className=" bg-slate-900 px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8">
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
